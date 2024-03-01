132258edc4e44295ae26f4c91991d974196e6d77
gonsmoke <uouo721125@yahoo.com.tw>
2024-03-02 00:05:46 +0800

    2024/3/2

--- 精通python.py
+++ 精通python.py
@@ -1348,15 +1348,34 @@ life = {
 
 # print(good())
 ##############################9.2
-def get_odds():
-    for number in range(1, 10, 2):
-        yield number
-count = 1
-for number in get_odds():
+# def get_odds():
+#     for number in range(1, 10, 2):
+#         yield number
+# count = 1
+# for number in get_odds():
   
-    if count == 3:
-        print("The third odd number is ", number)
-        break
-    count += 1 #count寫在上面比較慢一步到達
-   
+#     if count == 3:
+#         print("The third odd number is ", number)
+#         break
+#     count += 1 #count寫在上面比較慢一步到達
+###############################9.3
+def test(func):
+    def new_func(*args, **kwargs):
+        print("start")
+        result = func(*args, **kwargs)  
+        print("end")
+        return result
+    return new_func
+@test  ##用裝飾品才能呼叫 def test
+def greeting():
+    print("Greetings, Earthling")
+greeting()
+# ###############################9.4
+# class OopsException(Exception):  ###一定定義OopsException然後pass
+#     pass
+# try:
+#     raise OopsException()
+# except OopsException:
+#     print("Caught an oops")
 
+#################################176
